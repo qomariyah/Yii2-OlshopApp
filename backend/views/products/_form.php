009f16fd5d836a96076f91e1e42c16d5a2cadede
qomariyah <qomariyah2211@gmail.com>
2018-10-12 10:10:01 +0700

    Olshop app with Yii2

---
+++ backend/views/products/_form.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Products */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="products-form">
+
+    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'amount')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'quantity')->textInput() ?>
+
+    <?= $form->field($model, 'avaibility')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'prod_condition')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'brand')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'stock')->textInput() ?>
+
+    <?= $form->field($model, 'image')->fileInput() ?>
+
+    <?= $form->field($model, 'status')->dropDownList([1 => 'Active', 0 => 'Non-Active']) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
